d51f96566663e09b5185bf4a6c3b43eae250a8a0
Mark capati <mmacapati@tip.edu.ph>
2025-11-22 22:13:22 +0800

    filter

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,15 +1,40 @@
+// models/Post.js
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  gameId: { type: mongoose.Schema.Types.ObjectId, ref: "Game", required: true },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  gameId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Game",
+    required: true,
+  },
   rank: { type: String, required: true },
-  team: { type: String, enum: ["Duo", "Trio", "Any"], required: true },
-  vibe: { type: String, enum: ["Casual", "Fun", "Competitive"], required: true },
-  mic: { type: String, enum: ["Open Mic", "Close Mic"], required: true },
+  team: {
+    type: String,
+    enum: ["Duo", "Trio", "Five", "Any"],
+    required: true,
+  },
+  vibe: {
+    type: String,
+    enum: ["Casual", "Fun", "Competitive"],
+    required: true,
+  },
+  mic: {
+    type: String,
+    enum: ["Open Mic", "Close Mic"],
+    required: true,
+  },
   partyCode: { type: String },
-  userRating: { type: Number, min: 1, max: 5, required: true }, // Added to store user's rating
-  createdAt: { type: Date, default: Date.now },
+  userRating: { type: Number, min: 1, max: 5, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600, // Auto delete after exactly 1 hour
+  },
 });
 
 module.exports = mongoose.model("Post", postSchema);
